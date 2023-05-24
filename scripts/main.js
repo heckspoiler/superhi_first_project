fb35396f0f6ce60b69520051a185466e0d6dd644
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-24 14:54:47 +0200

    starting again

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,15 @@ const body = document.querySelector("body");
 
 const next = function () {
   pageNumber = pageNumber + 1;
+
   updateSection();
 };
 
 const updateSection = function () {
   outputTag.innerHTML = pageNumber;
+  if (pageNumber > 10) {
+    outputTag.innerHTML = "Stop clicking, it won't get better";
+  }
 };
 
 nextTag.addEventListener("click", next);
