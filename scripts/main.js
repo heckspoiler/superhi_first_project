0c5dce7e291290377350122b0e0fc1732af8a033
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-24 12:14:31 +0200

    lesson finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
-const title = document.querySelector("h2");
+const tag = document.querySelector("h2");
+const nextTag = document.querySelector("footer img.next");
+const body = document.querySelector("body");
 
-const changeTitle = function () {};
+tag.innerHTML = "this is a test";
+
+tag.style.color = "red";
+
+const updateTag = function () {
+  tag.innerHTML = "fucking your face";
+  tag.style.color = "blue";
+  body.style.backgroundColor = "violet";
+};
+
+nextTag.addEventListener("click", updateTag);
